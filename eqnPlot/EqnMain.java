4ca77dd1dc23e03a04d9f16b0f44dc0b829ca36b
salamander2 <mk_gecko@yahoo.com>
2019-07-04 17:48:01 -0400

    Create EqnMain.java

---
+++ eqnPlot/EqnMain.java
@@ -0,0 +1,14 @@
+package eqnPlot;
+
+/* For some reason I split this up into 4 classes. Probably to illustrate something about classes.
+ * It could easily all be one file and still be understandable.
+ */
+
+//This is the main program that runs.
+public class EqnMain {
+
+	public static void main(String[] args) {
+		new EqFrame();
+	}
+
+}
